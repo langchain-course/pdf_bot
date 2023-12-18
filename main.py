6cb4a32d8e39f6c9aaef12fdbfe85858b652d21a
LiaoKong <568250549@qq.com>
2023-12-18 23:42:29 +0800

    final

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 
 import chainlit as cl
-from chainlit.types import AskFileResponse
 
 load_dotenv()
 
@@ -22,7 +21,8 @@ async def start():
         files = await cl.AskFileMessage(
             content='请上传你要提问的PDF文件',
             # 这里只运行 PDF 的文件
-            accept=["application/pdf"]
+            accept=["application/pdf"],
+            max_size_mb=3
         ).send()
 
     _file = files[0]
@@ -32,15 +32,16 @@ async def start():
     await msg.send()
 
     # 将上传的文件保存到服务器本地
-    file_path = (f'./tmp/{_file.name}')
-    open(file_path, 'wb').write(_file.content)
+    file_path = f'./tmp/{_file.name}'
+    with open(file_path, 'wb') as f:
+        f.write(_file.content)
 
     # 加载 PDF 文档
     docs = PyMuPDFLoader(file_path).load()
 
     # 分割文档
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000, chunk_overlap=100
+        chunk_size=800, chunk_overlap=100
     )
     split_docs = text_splitter.split_documents(docs)
 
@@ -64,12 +65,43 @@ async def start():
         chain_type='stuff',
         retriever=docsearch.as_retriever(),
         memory=memory,
+        # 因为我们需要将搜索到的结果当做来源展示到页面上，
+        # 所以这里需要设置为 True
+        return_source_documents=True,
     )
 
     msg.content = f'`{_file.name}` 处理完成，请开始你的问答。'
     await msg.update()
 
-# @cl.on_message
-# async def process_response(res):
-#     answer = res["answer"]
-#     await cl.Message(content=answer).send()
+    # 将 chain 对象保存到用户 session 中
+    cl.user_session.set("chain", chain)
+
+
+@cl.on_message
+async def main(message: cl.Message):
+    # 获取在初始化时存储的 Chain 对象
+    chain = cl.user_session.get('chain')
+
+    # AsyncLangchainCallbackHandle 会将执行时中间步骤实时显示到聊天列表中
+    cb = cl.AsyncLangchainCallbackHandler()
+    # 进行检索
+    res = await chain.acall(message.content, callbacks=[cb])
+    answer = res['answer']
+    source_documents = res['source_documents']
+
+    # 将检索到的内容一并显示到界面上
+    text_elements = []
+    if source_documents:
+        for index, source_doc in enumerate(source_documents):
+            source_name = f'来源{index + 1}'
+            text_elements.append(
+                cl.Text(content=source_doc.page_content, name=source_name)
+            )
+        source_names = [text_el.name for text_el in text_elements]
+
+        if source_names:
+            answer += f'\n\n来源: {", ".join(source_names)}'
+        else:
+            answer += '\n\n来源未找到'
+
+    await cl.Message(content=answer, elements=text_elements).send()
